a7cae2ceb416a73e9fe49b261f2d3222db894448
Rudransh Agrawal <rudranshagrawal@MacBook-Air.local>
2025-05-27 18:01:45 -0500

    Fix base path for Netlify deploy

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: './', // ✅ This is the fix for blank screen on Netlify
   plugins: [react()],
-})
+});
